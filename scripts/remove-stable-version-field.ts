0286b6478f34136f87dfba7f85f5d1ca230f0851
Vivian Plasencia <v.pcalana@gmail.com>
2025-03-31 12:00:32 +0200

    refactor: update variable name

--- scripts/remove-stable-version-field.ts
+++ scripts/remove-stable-version-field.ts
@@ -3,9 +3,9 @@ import { existsSync, readFileSync, writeFileSync } from "node:fs"
 async function main() {
     let dotIndex = process.argv[2].lastIndexOf(".")
 
-    let packageName = dotIndex !== -1 ? process.argv[2].slice(0, dotIndex) : process.argv[2]
+    let folderName = dotIndex !== -1 ? process.argv[2].slice(0, dotIndex) : process.argv[2]
 
-    const projectDirectory = `packages/${packageName}`
+    const projectDirectory = `packages/${folderName}`
 
     let filePath = `${projectDirectory}/package.json`
 
